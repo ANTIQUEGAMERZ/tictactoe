ff48979a1b2fc122b2be469cf317fea10b720c00
ANTIQUEGAMERZ <zxy3455@gmail.com>
2024-08-12 20:15:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function cheakWin(){
         [1, 5, 8], [2, 4, 7], [1, 3, 6],
         [3, 7, 8], [6, 4, 5], [0, 4, 5],
         [2, 4, 3], [3, 4, 8], [1, 4, 8],
-        [0, 3, 7],
+        [0, 3, 7], [5, 6, 7],
     ]
     for(let i = 0; i<winConditions.length; i++){
         let v0 = boxes[winConditions[i][0]].innerHTML;
